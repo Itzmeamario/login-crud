55ca44fd233dad7d4d4c32bd39749d2515162586
Mario <mario.santamaria.portilla@gmail.com>
2018-09-30 20:28:07 -0500

    reworked to stateless comp

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -3,57 +3,46 @@ import Register from './Register';
 import UserList from './UserList';
 import UpdateUser from './UpdateUser';
 
-class Display extends Component {
-  constructor(props) {
-    super(props);
+const display = ({ selectedOption }) => {
+  let reveal = null;
 
-    this.state = {
-
-    };
-
-  }
-
-  render() {
-    let reveal = null;
-
-    if(this.props.selectedOption === 0) {
-      reveal = (
-        <div>
-          <h3>
-            View All
-          </h3>
-          <UserList />
-        </div>
-      );
-    } else if(this.props.selectedOption === 1) {
-      reveal = (
-        <div>
-          <h3>
-            Create User
-          </h3>
-          <Register 
-            mode={0}
-          />
-        </div>
-      );
-    } else if(this.props.selectedOption === 2) {
-      reveal = (
-        <div>
-          <h3>
-            Update User
-          </h3>
-          <UpdateUser />
-        </div>
-      );
-    }
-
-    return (
+  if(selectedOption === 0) {
+    reveal = (
       <div>
-        Display
-        { reveal }
+        <h3>
+          View All
+        </h3>
+        <UserList />
+      </div>
+    );
+  } else if(selectedOption === 1) {
+    reveal = (
+      <div>
+        <h3>
+          Create User
+        </h3>
+        <Register 
+          mode={0}
+        />
+      </div>
+    );
+  } else if(selectedOption === 2) {
+    reveal = (
+      <div>
+        <h3>
+          Update User
+        </h3>
+        <UpdateUser />
       </div>
     );
   }
+
+  return (
+    <div>
+      Display
+      { reveal }
+    </div>
+  );
 }
 
-export default Display;
+export default display;
